b7c75628b7af098d85bef0e8f18584c640055121
Markandey Shukla <117027551+markandeyshukla@users.noreply.github.com>
2025-11-05 17:21:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+function leapyear(year) {
+	    if(year%4==0 !! ( year%400==0 && year%100!=0)){
+			console.log("leap year having 366 days")
+		}else{
+			console.log("not a leap year having 365 days")
+		}
+}
